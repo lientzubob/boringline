526519235d4a54b2c62830899a4799d410b004e7
lien <lien@MSI>
2018-07-06 14:39:16 +0800

    新增資料庫資料import.sql 設定為create自動資料庫重整

---
+++ boringline/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO `boringline`.`message` (`message_account`, `message_article`) VALUES ('bob', 'fuckyou');
+INSERT INTO `boringline`.`message` (`message_account`, `message_article`) VALUES ('jack', 'fuckme');
+INSERT INTO `boringline`.`message` (`message_account`, `message_article`) VALUES ('nathan', 'fuckhim');
+INSERT INTO `boringline`.`message` (`message_account`, `message_article`) VALUES ('jerry', 'fuckwho');
+INSERT INTO `boringline`.`message` (`message_account`, `message_article`) VALUES ('tom', 'fuckuaua');
+INSERT INTO `boringline`.`message` (`message_account`, `message_article`) VALUES ('tim', 'fuckyaya');
