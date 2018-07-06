63ae2f36350497507b2354670985f09db4049ba1
strike5931 <35601322+strike5931@users.noreply.github.com>
2018-07-06 11:45:16 +0800

    123木頭人

--- boringline/src/main/java/com/casual/CasualMainApplication.java
+++ boringline/src/main/java/com/casual/CasualMainApplication.java
@@ -2,8 +2,11 @@ package com.casual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class CasualMainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CasualMainApplication.class, args);
