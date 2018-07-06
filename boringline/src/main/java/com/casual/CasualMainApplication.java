db80c3097ca414decb295e2ad9f1b37922256e78
lien <lien@MSI>
2018-07-06 12:28:03 +0800

    update main

--- boringline/src/main/java/com/casual/CasualMainApplication.java
+++ boringline/src/main/java/com/casual/CasualMainApplication.java
@@ -2,11 +2,9 @@ package com.casual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class CasualMainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CasualMainApplication.class, args);
